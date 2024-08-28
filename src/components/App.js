8b8285a26f3654ae8ef17e2da941f73d2eaf4ec8
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-08-28 18:22:38 +0000

    a

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import UserList from './UserList';
+import UserDetail from './UserDetail';
 import './../styles/App.css';
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={UserList} />
+        <Route path="/user/:userId" component={UserDetail} />
+      </Switch>
+    </Router>
   )
 }
 
---
+++ src/components/UserDetail.js
@@ -0,0 +1,25 @@
+// src/UserDetail.js
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { users } from './users';
+
+const UserDetail = () => {
+  const { userId } = useParams();
+  const user = users.find(user => user.id === parseInt(userId));
+
+  if (!user) {
+    return <h2>User not found</h2>;
+  }
+
+  return (
+    <div>
+      <h1>{user.name}</h1>
+      <p>Email: {user.email}</p>
+      <p>Phone: {user.phone}</p>
+      <p>Website: {user.website}</p>
+      <Link to="/">Back to User List</Link>
+    </div>
+  );
+};
+
+export default UserDetail;
