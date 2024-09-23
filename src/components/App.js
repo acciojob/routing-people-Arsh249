a08371cbbf3c0c940695eac1c1b13373033c0b1e
Arsh Ilahi <arshilahi74@gmail.com>
2024-09-23 16:33:16 +0530

    k

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
-import React from "react";
 import 'regenerator-runtime/runtime';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";  // Use `Routes` instead of `Switch`
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import UserList from "./UserList";
 import UserDetail from "./UserDetail";
+
+const NotFound = () => <div>Loading...</div>;
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        {/* Use element prop and pass components as JSX */}
         <Route path="/" element={<UserList />} />
         <Route path="/users/:userId" element={<UserDetail />} />
+        {/* Fallback route for invalid URLs */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
